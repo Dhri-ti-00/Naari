fade14f7ec6e11ef6884ec1091868e38bd1bcfbe
Dhri-ti-00 <ghosh.dhritisha108@gmail.com>
2023-03-23 01:27:55 +0600

    new2

---
+++ app/jv1.java
@@ -0,0 +1,15 @@
+public class jv1 {
+    public jv1(String jv1) {
+        this.jv1 = jv1;
+    }
+
+    public String getJv1() {
+        return jv1;
+    }
+
+    public void setJv1(String jv1) {
+        this.jv1 = jv1;
+    }
+
+    private String jv1;
+}
