9821575795e6f96695ae5f8edb50850844650092
Dhri-ti-00 <ghosh.dhritisha108@gmail.com>
2023-03-23 01:46:29 +0600

    new

---
+++ app/src/test/java/nr.java
@@ -0,0 +1,15 @@
+public class nr {
+    public String getNr() {
+        return nr;
+    }
+
+    public void setNr(String nr) {
+        this.nr = nr;
+    }
+
+    public nr(String nr) {
+        this.nr = nr;
+    }
+
+    String nr;
+}
