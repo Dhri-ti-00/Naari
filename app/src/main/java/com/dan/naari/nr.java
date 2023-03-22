39ba63261e102ce13f206a6af6aec41f56e1a61f
Dhri-ti-00 <ghosh.dhritisha108@gmail.com>
2023-03-23 02:00:00 +0600

    new

--- app/src/main/java/com/dan/naari/nr.java
+++ app/src/main/java/com/dan/naari/nr.java
@@ -1,16 +1,16 @@
 package com.dan.naari;
 
 public class nr {
-    public nr(String nr) {
-        this.nr1 = nr;
+    public nr(String nr1) {
+        this.nr1 = nr1;
     }
 
     public String getNr() {
         return nr1;
     }
 
-    public void setNr(String nr) {
-        this.nr1 = nr;
+    public void setNr(String nr1) {
+        this.nr1 = nr1;
     }
 
     private String nr1;
